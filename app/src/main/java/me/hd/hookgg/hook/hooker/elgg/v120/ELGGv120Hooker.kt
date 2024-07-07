701c518a73cd50b13f0d4ac0f60f241b4707f665
hd <1839732296@qq.com>
2024-07-07 18:36:45 +0800

    feat: isPackageInstalled on elgg(1.1.4, 1.2.0)

--- app/src/main/java/me/hd/hookgg/hook/hooker/elgg/v120/ELGGv120Hooker.kt
+++ app/src/main/java/me/hd/hookgg/hook/hooker/elgg/v120/ELGGv120Hooker.kt
@@ -2,8 +2,8 @@ package me.hd.hookgg.hook.hooker.elgg.v120
 
 import com.highcapable.yukihookapi.hook.factory.method
 import kotlinx.coroutines.launch
-import me.hd.hookgg.data.func.GG
 import me.hd.hookgg.data.SetPrefsData
+import me.hd.hookgg.data.func.GG
 import me.hd.hookgg.hook.base.BaseGGHooker
 import me.hd.hookgg.hook.hooker.elgg.v120.ELGGv120VarArgs.arg
 import me.hd.hookgg.hook.hooker.elgg.v120.ELGGv120VarArgs.checkint
@@ -19,23 +19,24 @@ import me.hd.hookgg.hook.utils.GGUtil
 
 object ELGGv120Hooker : BaseGGHooker() {
     override val functionMap = mapOf(
-        GG.addListItems to { addListItems() },
-        GG.alert to { alert() },
-        GG.choice to { choice() },
-        GG.clearResults to { clearResults() },
-        GG.editAll to { editAll() },
-        GG.getRangesList to { getRangesList() },
-        GG.getResults to { getResults() },
-        GG.getResultsCount to { getResultsCount() },
-        GG.getValues to { getValues() },
-        GG.makeRequest to { makeRequest() },
-        GG.multiChoice to { multiChoice() },
-        GG.prompt to { prompt() },
-        GG.searchNumber to { searchNumber() },
-        GG.searchPointer to { searchPointer() },
-        GG.setRanges to { setRanges() },
-        GG.setValues to { setValues() },
-        GG.toast to { toast() }
+        GG.addListItems to { this.addListItems() },
+        GG.alert to { this.alert() },
+        GG.choice to { this.choice() },
+        GG.clearResults to { this.clearResults() },
+        GG.editAll to { this.editAll() },
+        GG.getRangesList to { this.getRangesList() },
+        GG.getResults to { this.getResults() },
+        GG.getResultsCount to { this.getResultsCount() },
+        GG.getValues to { this.getValues() },
+        GG.isPackageInstalled to { this.isPackageInstalled() },
+        GG.makeRequest to { this.makeRequest() },
+        GG.multiChoice to { this.multiChoice() },
+        GG.prompt to { this.prompt() },
+        GG.searchNumber to { this.searchNumber() },
+        GG.searchPointer to { this.searchPointer() },
+        GG.setRanges to { this.setRanges() },
+        GG.setValues to { this.setValues() },
+        GG.toast to { this.toast() }
     )
 
     private fun addListItems() {
@@ -207,6 +208,24 @@ object ELGGv120Hooker : BaseGGHooker() {
                     val values = varArgs.checktable(1)
                     scope.launch {
                         val func = "gg.getValues($values)"
+                        sendLog(func, result)
+                    }
+                }
+            }.ignoredAllFailure()
+        }
+    }
+
+    private fun isPackageInstalled() {
+        "android.ext.ۦۤۡۤ".toClassOrNull()?.apply {
+            method {
+                name = "b"
+                paramCount = 1
+            }.ignored().hook {
+                after {
+                    val varArgs = args(0).any()
+                    val pkg = varArgs.checkjstring(1)
+                    scope.launch {
+                        val func = "gg.isPackageInstalled($pkg)"
                         sendLog(func, result)
                     }
                 }
