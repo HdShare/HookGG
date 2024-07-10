76f7284c0d536f4a5f936ce7337381fb6fb471a6
hd <1839732296@qq.com>
2024-07-10 19:11:34 +0800

    fix: btn nav label mode

--- app/src/main/java/me/hd/hookgg/ui/fragment/MainNavFragment.kt
+++ app/src/main/java/me/hd/hookgg/ui/fragment/MainNavFragment.kt
@@ -3,7 +3,6 @@ package me.hd.hookgg.ui.fragment
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import me.hd.hookgg.R
 import me.hd.hookgg.databinding.FragmentMainNavBinding
 import me.hd.hookgg.ui.fragment.base.BaseFragment
@@ -14,8 +13,6 @@ class MainNavFragment : BaseFragment<FragmentMainNavBinding, ViewModel>(
 ) {
     override fun initFragment(binding: FragmentMainNavBinding, viewModel: ViewModel?) {
         (childFragmentManager.findFragmentById(R.id.mainNavView) as NavHostFragment).apply {
-            binding.mainNavBottomNav.labelVisibilityMode =
-                BottomNavigationView.LABEL_VISIBILITY_SELECTED
             binding.mainNavBottomNav.setupWithNavController(navController)
         }
     }
