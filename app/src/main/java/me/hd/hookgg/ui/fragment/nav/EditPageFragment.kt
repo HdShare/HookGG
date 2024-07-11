2aaefdd92bf2d59e1f459ab783f74ecfb63ad8dd
hd <1839732296@qq.com>
2024-07-12 07:56:38 +0800

    feat: init sora code editor

--- app/src/main/java/me/hd/hookgg/ui/fragment/nav/EditPageFragment.kt
+++ app/src/main/java/me/hd/hookgg/ui/fragment/nav/EditPageFragment.kt
@@ -3,9 +3,18 @@ package me.hd.hookgg.ui.fragment.nav
 import android.graphics.Typeface
 import androidx.lifecycle.ViewModel
 import com.highcapable.yukihookapi.YukiHookAPI
+import io.github.rosemoe.sora.langs.textmate.TextMateColorScheme
+import io.github.rosemoe.sora.langs.textmate.TextMateLanguage
+import io.github.rosemoe.sora.langs.textmate.registry.FileProviderRegistry
+import io.github.rosemoe.sora.langs.textmate.registry.GrammarRegistry
+import io.github.rosemoe.sora.langs.textmate.registry.ThemeRegistry
+import io.github.rosemoe.sora.langs.textmate.registry.dsl.languages
+import io.github.rosemoe.sora.langs.textmate.registry.model.ThemeModel
+import io.github.rosemoe.sora.langs.textmate.registry.provider.AssetsFileResolver
 import me.hd.hookgg.R
 import me.hd.hookgg.databinding.FragmentEditPageBinding
 import me.hd.hookgg.ui.fragment.base.BaseFragment
+import org.eclipse.tm4e.core.registry.IThemeSource
 
 class EditPageFragment : BaseFragment<FragmentEditPageBinding, ViewModel>(
     FragmentEditPageBinding::inflate,
@@ -23,7 +32,58 @@ class EditPageFragment : BaseFragment<FragmentEditPageBinding, ViewModel>(
     }
 
     private fun initCodeEditor(binding: FragmentEditPageBinding) {
-        val typeface = Typeface.createFromAsset(requireContext().assets, "font-mono.ttf")
-        binding.editPageEditor.typefaceText = typeface
+        val typeface =
+            Typeface.createFromAsset(requireContext().assets, "font/JetBrainsMono-Regular.ttf")
+        binding.editPageCodeEditor.typefaceText = typeface
+
+        val symbolInputView = binding.editPageSymbolInputView
+        symbolInputView.bindEditor(binding.editPageCodeEditor)
+        symbolInputView.addSymbols(
+            arrayOf(
+                "(", ")", "[", "]", "{", "}", "\"", "=", ":", ".", ",", ":", "_", "+", "-",
+                "*", "/", "\\", "%", "#", "^", "$", "?", "&", "|", "<", ">", "~", "'"
+            ),
+            arrayOf(
+                "(", ")", "[", "]", "{", "}", "\"", "=", ":", ".", ",", ":", "_", "+", "-",
+                "*", "/", "\\", "%", "#", "^", "$", "?", "&", "|", "<", ">", "~", "'"
+            )
+        )
+
+        FileProviderRegistry.getInstance().addFileProvider(
+            AssetsFileResolver(
+                requireContext().assets
+            )
+        )
+
+        val path = "textmate/quietlight.json"
+        ThemeRegistry.getInstance().loadTheme(
+            ThemeModel(
+                IThemeSource.fromInputStream(
+                    FileProviderRegistry.getInstance().tryGetInputStream(path), path, null
+                ),
+                "quietlight"
+            )
+        )
+        ThemeRegistry.getInstance().setTheme("quietlight")
+
+        GrammarRegistry.getInstance().loadGrammars(
+            languages {
+                language("lua") {
+                    grammar = "textmate/lua/syntaxes/lua.tmLanguage.json"
+                    scopeName = "source.lua"
+                    languageConfiguration = "textmate/lua/language-configuration.json"
+                }
+            }
+        )
+
+        var editorColorScheme = binding.editPageCodeEditor.colorScheme
+        if (editorColorScheme !is TextMateColorScheme) {
+            editorColorScheme = TextMateColorScheme.create(ThemeRegistry.getInstance())
+            binding.editPageCodeEditor.colorScheme = editorColorScheme
+        }
+
+        val language = TextMateLanguage.create("source.lua", true)
+        binding.editPageCodeEditor.setEditorLanguage(language)
+
     }
 }
