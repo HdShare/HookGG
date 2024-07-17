c6ed8ef55317359ccb8bf509cce97960ec891687
hd <1839732296@qq.com>
2024-07-17 08:06:22 +0800

    test: update test entry

--- app/src/main/java/me/hd/hookgg/hook/hooker/test/TestEntry.kt
+++ app/src/main/java/me/hd/hookgg/hook/hooker/test/TestEntry.kt
@@ -5,6 +5,6 @@ import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 object TestEntry : YukiBaseHooker() {
     override fun onHook() {
         //loadHooker(DexkitHooker)
-        loadHooker(DaemonHooker)
+        //loadHooker(DaemonHooker)
     }
 }
