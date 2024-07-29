94aacb54b6c0913998581aa8c7c75393aabb704c
hd <1839732296@qq.com>
2024-07-29 18:03:55 +0800

    refactor: app data load logic

--- app/src/main/java/me/hd/hookgg/data/AppData.kt
+++ app/src/main/java/me/hd/hookgg/data/AppData.kt
@@ -1,33 +1,38 @@
 package me.hd.hookgg.data
 
+import me.hd.hookgg.hook.base.BaseGGHooker
 import me.hd.hookgg.hook.hooker.agg.v333.AGGv333Hooker
 import me.hd.hookgg.hook.hooker.elgg.v114.ELGGv114Hooker
 import me.hd.hookgg.hook.hooker.elgg.v120.ELGGv120Hooker
+import me.hd.hookgg.hook.hooker.elgg.v121.ELGGv121Hooker
 import me.hd.hookgg.hook.hooker.gg.v1011.GGv1011Hooker
 import me.hd.hookgg.hook.hooker.gg.v960.GGv960Hooker
 import me.hd.hookgg.hook.hooker.gg.v961.GGv961Hooker
 import me.hd.hookgg.hook.hooker.rlgg.v2092.RLGGv2092Hooker
 
 object AppData {
-    private fun getMapKey(map: Map<String, *>): Array<String> {
-        return map.keys.toTypedArray()
-    }
-
-    private val map = mapOf(
-        "GG 96.0" to getMapKey(GGv960Hooker.functionMap),
-        "GG 96.1" to getMapKey(GGv961Hooker.functionMap),
-        "GG 101.1" to getMapKey(GGv1011Hooker.functionMap),
-        "RLGG 2.0.9.2" to getMapKey(RLGGv2092Hooker.functionMap),
-        "ELGG 1.1.0~1.1.6" to getMapKey(ELGGv114Hooker.functionMap),
-        "ELGG 1.2.0" to getMapKey(ELGGv120Hooker.functionMap),
-        "AGG 3.3.3~3.3.91" to getMapKey(AGGv333Hooker.functionMap),
+    private val mapHooker = mapOf(
+        "GG 96.0" to GGv960Hooker,
+        "GG 96.1" to GGv961Hooker,
+        "GG 101.1" to GGv1011Hooker,
+        "RLGG 2.0.9.2" to RLGGv2092Hooker,
+        "ELGG 1.1.0~1.1.6" to ELGGv114Hooker,
+        "ELGG 1.2.0" to ELGGv120Hooker,
+        "ELGG 1.2.1" to ELGGv121Hooker,
+        "AGG 3.3.3~3.3.91" to AGGv333Hooker,
     )
 
+    fun getMapHooker(version: String): BaseGGHooker? {
+        return mapHooker[version]
+    }
+
     fun getVersionList(): Array<String> {
-        return getMapKey(map)
+        return mapHooker.keys.toTypedArray()
     }
 
     fun getFunctionList(version: String): Array<String> {
-        return map[version] ?: emptyArray()
+        return mapHooker[version]?.run {
+            functionMap.keys.toTypedArray()
+        } ?: arrayOf()
     }
 }
--- app/src/main/java/me/hd/hookgg/hook/HookEntry.kt
+++ app/src/main/java/me/hd/hookgg/hook/HookEntry.kt
@@ -8,13 +8,6 @@ import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 import me.hd.hookgg.BuildConfig
 import me.hd.hookgg.data.AppData
 import me.hd.hookgg.data.SetPrefsData
-import me.hd.hookgg.hook.hooker.agg.v333.AGGv333Hooker
-import me.hd.hookgg.hook.hooker.elgg.v114.ELGGv114Hooker
-import me.hd.hookgg.hook.hooker.elgg.v120.ELGGv120Hooker
-import me.hd.hookgg.hook.hooker.gg.v1011.GGv1011Hooker
-import me.hd.hookgg.hook.hooker.gg.v960.GGv960Hooker
-import me.hd.hookgg.hook.hooker.gg.v961.GGv961Hooker
-import me.hd.hookgg.hook.hooker.rlgg.v2092.RLGGv2092Hooker
 import me.hd.hookgg.hook.test.TestEntry
 
 @InjectYukiHookWithXposed(entryClassName = "Entry")
@@ -32,14 +25,8 @@ object HookEntry : IYukiHookXposedInit {
             if (prefs.get(SetPrefsData.PACKAGE_NAME, "") == packageName) {
                 loadApp(packageName) {
                     if (prefs.get(SetPrefsData.TEST_FUNC)) loadHooker(TestEntry)
-                    when (prefs.get(SetPrefsData.VERSION_NAME)) {
-                        AppData.getVersionList()[0] -> loadHooker(GGv960Hooker)
-                        AppData.getVersionList()[1] -> loadHooker(GGv961Hooker)
-                        AppData.getVersionList()[2] -> loadHooker(GGv1011Hooker)
-                        AppData.getVersionList()[3] -> loadHooker(RLGGv2092Hooker)
-                        AppData.getVersionList()[4] -> loadHooker(ELGGv114Hooker)
-                        AppData.getVersionList()[5] -> loadHooker(ELGGv120Hooker)
-                        AppData.getVersionList()[6] -> loadHooker(AGGv333Hooker)
+                    AppData.getMapHooker(prefs.get(SetPrefsData.VERSION_NAME))?.let { hooker ->
+                        loadHooker(hooker)
                     }
                 }
             }
