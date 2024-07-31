ada08ece02c6b9556bd23ff70bb8db11423681db
hd <1839732296@qq.com>
2024-07-31 23:52:33 +0800

    feat: gg 98.0

--- app/src/main/java/me/hd/hookgg/data/AppData.kt
+++ app/src/main/java/me/hd/hookgg/data/AppData.kt
@@ -10,20 +10,22 @@ import me.hd.hookgg.hook.hooker.elgg.v121.ELGGv121Hooker
 import me.hd.hookgg.hook.hooker.gg.v1011.GGv1011Hooker
 import me.hd.hookgg.hook.hooker.gg.v960.GGv960Hooker
 import me.hd.hookgg.hook.hooker.gg.v961.GGv961Hooker
+import me.hd.hookgg.hook.hooker.gg.v980.GGv980Hooker
 import me.hd.hookgg.hook.hooker.rlgg.v2092.RLGGv2092Hooker
 
 object AppData {
     private val mapHooker = mapOf(
-        "GG 96.0" to GGv960Hooker,
-        "GG 96.1" to GGv961Hooker,
-        "GG 101.1" to GGv1011Hooker,
-        "RLGG 2.0.9.2" to RLGGv2092Hooker,
-        "ELGG 1.1.0~1.1.6" to ELGGv114Hooker,
-        "ELGG 1.1.7" to ELGGv117Hooker,
-        "ELGG 1.1.9" to ELGGv119Hooker,
-        "ELGG 1.2.0" to ELGGv120Hooker,
-        "ELGG 1.2.1" to ELGGv121Hooker,
-        "AGG 3.3.3~3.3.91" to AGGv333Hooker,
+        "GG(96.0)" to GGv960Hooker,
+        "GG(96.1~97.0)" to GGv961Hooker,
+        "GG(98.0)" to GGv980Hooker,
+        "GG(99.0~101.1)" to GGv1011Hooker,
+        "RLGG(2.0.9.2)" to RLGGv2092Hooker,
+        "ELGG(1.1.0~1.1.6)" to ELGGv114Hooker,
+        "ELGG(1.1.7)" to ELGGv117Hooker,
+        "ELGG(1.1.9)" to ELGGv119Hooker,
+        "ELGG(1.2.0)" to ELGGv120Hooker,
+        "ELGG(1.2.1)" to ELGGv121Hooker,
+        "AGG(3.3.3~3.3.91)" to AGGv333Hooker,
     )
 
     fun getMapHooker(version: String): BaseGGHooker? {
