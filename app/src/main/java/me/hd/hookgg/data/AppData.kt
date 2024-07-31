cfd462a18fb74b896643adf5ab33c3ae002f96f0
hd <1839732296@qq.com>
2024-07-31 23:07:29 +0800

    feat: elgg 1.1.7

--- app/src/main/java/me/hd/hookgg/data/AppData.kt
+++ app/src/main/java/me/hd/hookgg/data/AppData.kt
@@ -3,6 +3,7 @@ package me.hd.hookgg.data
 import me.hd.hookgg.hook.base.BaseGGHooker
 import me.hd.hookgg.hook.hooker.agg.v333.AGGv333Hooker
 import me.hd.hookgg.hook.hooker.elgg.v114.ELGGv114Hooker
+import me.hd.hookgg.hook.hooker.elgg.v117.ELGGv117Hooker
 import me.hd.hookgg.hook.hooker.elgg.v119.ELGGv119Hooker
 import me.hd.hookgg.hook.hooker.elgg.v120.ELGGv120Hooker
 import me.hd.hookgg.hook.hooker.elgg.v121.ELGGv121Hooker
@@ -18,6 +19,7 @@ object AppData {
         "GG 101.1" to GGv1011Hooker,
         "RLGG 2.0.9.2" to RLGGv2092Hooker,
         "ELGG 1.1.0~1.1.6" to ELGGv114Hooker,
+        "ELGG 1.1.7" to ELGGv117Hooker,
         "ELGG 1.1.9" to ELGGv119Hooker,
         "ELGG 1.2.0" to ELGGv120Hooker,
         "ELGG 1.2.1" to ELGGv121Hooker,
