cd596701c666bec15782a408b7b954873a313da2
hd <1839732296@qq.com>
2024-08-02 11:34:00 +0800

    refactor: demo module globals test

--- app/src/main/java/me/hd/hookgg/hook/module/DemoModule.kt
+++ app/src/main/java/me/hd/hookgg/hook/module/DemoModule.kt
@@ -1,30 +1,53 @@
 package me.hd.hookgg.hook.module
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.classOf
 import com.highcapable.yukihookapi.hook.factory.method
+import luaj.Globals
+import luaj.LuaTable
 import luaj.LuaValue
-import luaj.lib.StringLib
-import luaj.lib.ZeroArgFunction
+import luaj.lib.OneArgFunction
+import luaj.lib.TwoArgFunction
+import java.util.Locale
 
-class MyStringLib {
-    class test : ZeroArgFunction() {
-        override fun call(): LuaValue {
-            return LuaValue.valueOf("测试")
+class TestLib : TwoArgFunction() {
+    override fun call(modname: LuaValue, env: LuaValue): LuaValue {
+        return LuaTable().also { testTable ->
+            testTable.set("toLower", toLower())
+            testTable.set("toUpper", toUpper())
+            env["test"] = testTable
+            if (env["package"].isnil().not()) {
+                env["package"]["loaded"].set("test", testTable)
+            }
+        }
+    }
+
+    internal class toLower : OneArgFunction() {
+        override fun call(arg: LuaValue): LuaValue {
+            return LuaValue.valueOf(arg.checkjstring().lowercase(Locale.getDefault()))
+        }
+    }
+
+    internal class toUpper : OneArgFunction() {
+        override fun call(arg: LuaValue): LuaValue {
+            return LuaValue.valueOf(arg.checkjstring().uppercase(Locale.getDefault()))
         }
     }
 }
 
 object DemoModule : YukiBaseHooker() {
     override fun onHook() {
-        StringLib::class.java.apply {
+        "android.ext.Script".toClassOrNull()?.apply {
             method {
-                name = "call"
-                param(classOf<LuaValue>(), classOf<LuaValue>())
+                name = "init"
+                emptyParam()
             }.ignored().hook {
                 after {
-                    val luaValue = result as LuaValue
-                    luaValue.set("test", MyStringLib.test())
+                    val field = instance::class.java
+                        .declaredFields
+                        .first { it.name == "globals" }
+                        .apply { isAccessible = true }
+                    val globals = field.get(instance) as Globals
+                    globals.load(TestLib())
                 }
             }
         }
