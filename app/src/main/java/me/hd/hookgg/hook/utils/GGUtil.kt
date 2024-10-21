c45b09cf63fb6d51d7947232de77f136aa1d7872
hd <1839732296@qq.com>
2024-10-21 15:23:01 +0800

    fix: isValidParams logic

--- app/src/main/java/me/hd/hookgg/hook/utils/GGUtil.kt
+++ app/src/main/java/me/hd/hookgg/hook/utils/GGUtil.kt
@@ -9,13 +9,14 @@ object GGUtil {
 
     fun isValidParams(text: String): Boolean {
         val isValidLength = text.length in 1..100
+        if (!isValidLength) return false
         val isSearchNumber = text.matches(Regex("^\\s*[0-9\\s]+\\s*$"))
         val isSearchTextUtf = setOf(":", ";").any { prefix -> text.startsWith(prefix) }
         val isSearchHex = setOf("h").any { prefix -> text.startsWith(prefix) }
         val isSearchEndian = setOf("h", "r").any { prefix -> text.endsWith(prefix) }
         val isSearchHexUtf = setOf("Q", "q").any { prefix -> text.startsWith(prefix) }
         val isSearchATA8 = setOf("~A ", "~T ", "~A8 ").any { prefix -> text.startsWith(prefix) }
-        return isValidLength && (isSearchNumber || isSearchTextUtf || isSearchHex || isSearchEndian || isSearchHexUtf || isSearchATA8)
+        return isSearchNumber || isSearchTextUtf || isSearchHex || isSearchEndian || isSearchHexUtf || isSearchATA8
     }
 
     fun getConstValue(constMap: Map<String, Int>, value: Int): String {
