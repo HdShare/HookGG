92ed60d49f31925fd1eb35c0ac81598afe9ab170
hd <1839732296@qq.com>
2024-10-29 00:11:38 +0800

    feat: add funcPage and setPage v0.1 icon

--- app/src/main/java/me/hd/hookgg/ui/fragment/MainNavFragment.kt
+++ app/src/main/java/me/hd/hookgg/ui/fragment/MainNavFragment.kt
@@ -7,6 +7,7 @@ import me.hd.hookgg.R
 import me.hd.hookgg.databinding.FragmentMainNavBinding
 import me.hd.hookgg.ui.adapter.MainViewPager2Adapter
 import me.hd.hookgg.ui.fragment.base.FragmentBase
+import me.hd.hookgg.ui.fragment.nav.FuncPageFragment
 import me.hd.hookgg.ui.fragment.nav.LogPageFragment
 import me.hd.hookgg.ui.fragment.nav.ModulePageFragment
 import me.hd.hookgg.ui.fragment.nav.SetPageFragment
@@ -23,6 +24,7 @@ class MainNavFragment : FragmentBase<FragmentMainNavBinding, ViewModel>(
                 arrayOf(
                     LogPageFragment(),
                     ModulePageFragment(),
+                    FuncPageFragment(),
                     SetPageFragment(),
                 )
             )
@@ -30,7 +32,8 @@ class MainNavFragment : FragmentBase<FragmentMainNavBinding, ViewModel>(
                 when (it.itemId) {
                     R.id.navLogPageFragment -> setCurrentItem(0, false)
                     R.id.navModulePageFragment -> setCurrentItem(1, false)
-                    R.id.navSetPageFragment -> setCurrentItem(2, false)
+                    R.id.navFuncPageFragment -> setCurrentItem(2, false)
+                    R.id.navSetPageFragment -> setCurrentItem(3, false)
                 }
                 true
             }
---
+++ app/src/main/java/me/hd/hookgg/ui/fragment/nav/FuncPageFragment.kt
@@ -0,0 +1,11 @@
+package me.hd.hookgg.ui.fragment.nav
+
+import androidx.lifecycle.ViewModel
+import me.hd.hookgg.databinding.FragmentFuncPageBinding
+import me.hd.hookgg.ui.fragment.base.FragmentBase
+
+class FuncPageFragment : FragmentBase<FragmentFuncPageBinding, ViewModel>(
+    FragmentFuncPageBinding::inflate
+) {
+
+}
