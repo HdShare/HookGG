fb23f1eab9c3590db8dd9637e47614e9fb0641d5
hd <hdshare@vip.qq.com>
2025-01-02 19:23:06 +0800

    refactor: remove elgg 1.1.0 ~ 1.1.7

--- app/src/main/java/me/hd/hookgg/config/data/HookerData.kt
+++ app/src/main/java/me/hd/hookgg/config/data/HookerData.kt
@@ -3,8 +3,6 @@ package me.hd.hookgg.config.data
 import me.hd.hookgg.config.bean.FuncObj
 import me.hd.hookgg.hook.base.BaseGGHooker
 import me.hd.hookgg.hook.hooker.agg.v333.AGGv333Hooker
-import me.hd.hookgg.hook.hooker.elgg.v114.ELGGv114Hooker
-import me.hd.hookgg.hook.hooker.elgg.v117.ELGGv117Hooker
 import me.hd.hookgg.hook.hooker.elgg.v119.ELGGv119Hooker
 import me.hd.hookgg.hook.hooker.elgg.v120.ELGGv120Hooker
 import me.hd.hookgg.hook.hooker.elgg.v121.ELGGv121Hooker
@@ -31,8 +29,6 @@ object HookerData {
         "GG [98.0]" to GGv980Hooker,
         "GG [99.0~101.1]" to GGv1011Hooker,
         "AGG [3.3.3~3.3.91]" to AGGv333Hooker,
-        "ELGG [1.1.0~1.1.6]" to ELGGv114Hooker,
-        "ELGG [1.1.7]" to ELGGv117Hooker,
         "ELGG [1.1.9]" to ELGGv119Hooker,
         "ELGG [1.2.0]" to ELGGv120Hooker,
         "ELGG [1.2.1]" to ELGGv121Hooker,
